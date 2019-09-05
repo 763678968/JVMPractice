a28650978041e8e2599627f0ca44f484959919c9
animation <763678968@qq.com>
2019-09-05 15:22:47 +0800

    被动引用1

---
+++ src/SuperClass.java
@@ -0,0 +1,22 @@
+public class SuperClass {
+
+    static {
+        System.out.println("SuperClass init!");
+    }
+
+    public static int value = 123;
+}
+
+class SubClass extends SuperClass {
+
+    static {
+        System.out.println("SubClass init!");
+    }
+}
+
+class NotInitialization {
+
+    public static void main(String[] args) {
+        System.out.println(SubClass.value);
+    }
+}
